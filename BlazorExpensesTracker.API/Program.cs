35f8d183875b7a8875b545a6e711a7f7a3d51faa
mi <mi@DESKTOP-OVUF872>
2023-10-12 12:12:31 +0100

    Configuring data access connection

--- BlazorExpensesTracker.API/Program.cs
+++ BlazorExpensesTracker.API/Program.cs
@@ -1,3 +1,6 @@
+using BlazorExpensesTracker.Data;
+using System.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+
+//connect to db
+var sqlConnectionConfiguration = new SqlConfiguration(builder.Configuration.GetConnectionString("DefaultConnection"));
+builder.Services.AddSingleton(sqlConnectionConfiguration);
 
 var app = builder.Build();
 
