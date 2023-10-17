12b70565a70d88bb5824ee276d4135b0d47c9e30
mi <mi@DESKTOP-OVUF872>
2023-10-17 12:44:21 +0100

    Validation for Datetime added

--- BlazorExpensesTracker.Model/Expense.cs
+++ BlazorExpensesTracker.Model/Expense.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using BlazorExpensesTracker.Model.validation;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,6 +16,8 @@ namespace BlazorExpensesTracker.Model
 
         public Category category { get; set; }
 
+        [Required]
+        [ExpenseTransactionDateValidator(DaysInTheFuture = 30)]
         public DateTime Transactiondate { get; set; }
 
         public ExpenseType ExpenseType { get; set; }
---
+++ BlazorExpensesTracker.Model/validation/ExpenseTransactionDateValidator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorExpensesTracker.Model.validation
+{
+    public class ExpenseTransactionDateValidator : ValidationAttribute
+    {
+        public int DaysInTheFuture { get; set; }
+        protected override ValidationResult IsValid(object? value, ValidationContext validationContext)
+        {
+            DateTime transactionDate;
+
+            //check that is a date
+            if (DateTime.TryParse(value.ToString(), out transactionDate))
+            {
+                //minvalue means date may be empty
+                if (transactionDate == DateTime.MinValue)
+                {
+                    return new ValidationResult($"Date shouldn't be empty.",
+                        new[] { validationContext.MemberName });
+                }
+                else if (transactionDate > DateTime.Now.AddDays(DaysInTheFuture))
+                {
+                    return new ValidationResult($"Date can't be greater than today plus {DaysInTheFuture}",
+                        new[] { validationContext.MemberName });
+                }
+                //validation pases, so return null
+                return null;
+            }
+            return new ValidationResult("Invalid Date",
+            new[] { validationContext.MemberName });
+        }
+    }
+}
