06ae42265fd0dc6f4759724739d33a515d7f9676
mi <mi@DESKTOP-OVUF872>
2023-10-17 08:57:56 +0100

    Model Expense and ExpenseType created

---
+++ BlazorExpensesTracker.Model/Expense.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorExpensesTracker.Model
+{
+    internal class Expense
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public string CategoryId { get; set; }
+
+        public Category category { get; set; }
+
+        public DateTime Transactiondate { get; set; }
+
+        public ExpenseType ExpenseType { get; set; }
+
+    }
+}
